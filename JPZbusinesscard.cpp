60112c5f64d6cd43b420d20472a23baefd708e85
jpaz26 <47163896+jpaz26@users.noreply.github.com>
2019-02-07 22:56:28 -0600

    c++ business card code

---
+++ JPZbusinesscard.cpp
@@ -0,0 +1,24 @@
+/* 
+Joshua paz
+
+Business Card.cpp : Defines the entry point for the console application.
+
+*/
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	
+	//Prints out business card information
+	cout << "Joshua Paz" << endl;
+	cout << "EA Games QA Dept." << endl;
+	cout << "jpz@ea.qa" << endl;
+	cout << "210-244-4648" << endl;
+
+    return 0;
+}
+
