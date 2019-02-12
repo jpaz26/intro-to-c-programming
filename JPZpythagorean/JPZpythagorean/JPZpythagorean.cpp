0e96061c88260436d4bbb3a82dfed3aa3a16eb11
jpaz26 <47163896+jpaz26@users.noreply.github.com>
2019-02-12 17:52:49 -0600

    Add files via upload

---
+++ JPZpythagorean/JPZpythagorean/JPZpythagorean.cpp
@@ -0,0 +1,27 @@
+// JPZpythagorean.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+using namespace std;
+#include <math.h>
+#include <iostream>
+
+
+int main()
+{
+	// create variables for a,b, and c
+	double a, b, c;
+	//print out text prompting user input for variables
+	cout << "Enter a and b" << endl;
+	//input code for a and b
+	cin >> a >> b;
+	// pythagorean equation
+	c = a*a + b*b;
+	c = sqrt(c);
+	//print out solution
+	cout << "c =" << " " << c << endl;
+
+
+    return 0;
+}
+
