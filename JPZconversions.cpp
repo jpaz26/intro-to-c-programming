144150e20a0a1a582d782877811a908a199438fe
jpaz26 <47163896+jpaz26@users.noreply.github.com>
2019-02-12 16:43:14 -0600

    Update JPZconversions.cpp

--- JPZconversions.cpp
+++ JPZconversions.cpp
@@ -11,13 +11,21 @@ using namespace std;
 
 int main()
 {
+	// create variable for miles
 	float miles;
+	// print out text for user to input distance
 	cout <<"Enter Distance in Miles"<< endl;
+	// create the input for miles
 	cin >> miles;
+	// declare kilometers as a variable
 	int kilometers;
+	// print out the text for the conversion
 	cout << "Distance in Kilometers" << endl;
+	// the conversion expression
 	kilometers = miles * M;
+	// output the converted distance
 	cout << kilometers;
+	// empty space holder
 	cout << "" << endl;
     return 0;
 }
